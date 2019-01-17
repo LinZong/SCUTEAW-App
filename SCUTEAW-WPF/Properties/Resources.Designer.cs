7c5e937bff601a53a78e425f33564c8b45ff8100
Nemesiss.Lin <353388639@qq.com>
2019-01-17 20:37:30 +0800

    Adjust row height and add an icon.

--- SCUTEAW-WPF/Properties/Resources.Designer.cs
+++ SCUTEAW-WPF/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace SCUTEAW_App.Properties {
         }
         
         /// <summary>
+        ///   查找类似于 (图标) 的 System.Drawing.Icon 类型的本地化资源。
+        /// </summary>
+        public static System.Drawing.Icon EnChan {
+            get {
+                object obj = ResourceManager.GetObject("EnChan", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   查找类似 JSESSION shouldn&apos;t be empty. 的本地化字符串。
         /// </summary>
         public static string NeedJSESSION {
